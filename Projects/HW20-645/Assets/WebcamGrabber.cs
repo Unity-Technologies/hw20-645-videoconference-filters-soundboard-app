4e47d1aee02f0f400c77fded4944a2714a41104b
Jean Blouin <jean@unity3d.com>
2020-06-22 11:00:55 -0400

    Initial HDRP project

---
+++ Projects/HW20-645/Assets/WebcamGrabber.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamGrabber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
