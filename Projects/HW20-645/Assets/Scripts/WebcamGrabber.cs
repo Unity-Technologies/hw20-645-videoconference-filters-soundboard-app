2d6f2cb6922b3f761278bc90d1e61873fe3cafea
Anthony <vincismurf@gmail.com>
2020-06-22 13:59:43 -0500

    Added Spout support
    Updated package and example scene to hook into spout camera

---
+++ Projects/HW20-645/Assets/Scripts/WebcamGrabber.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamGrabber : MonoBehaviour
+{
+    public RenderTexture targetTexture;
+    public int width = 1920, height = 1080, fps = 30;
+    WebCamTexture webcamTexture;
+
+    void Start()
+    {
+        SetWebCamTexture();
+    }
+
+    void Update()
+    {
+        Graphics.Blit(webcamTexture, targetTexture);
+    }
+
+    void SetWebCamTexture()
+    {
+        if (webcamTexture != null && webcamTexture.isPlaying)
+        {
+            webcamTexture.Stop();
+        }
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        webcamTexture = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
+        webcamTexture.Play();
+    }
+}
